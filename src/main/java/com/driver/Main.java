909f515d2c655732ca0c7e4bc8561b1271e048fb
Surandan <110124420+Surandan@users.noreply.github.com>
2023-06-16 14:52:11 +0530

    afterstaticuse

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 public class Main {
+    public static class A {
+        public static String meth() {
+            return "Invoking method from class A";
+        }
+    }
+    public static class B extends A{
+        public static String meth() {
+            return "Method is overridden in Extendend class B";
+        }
+    }
+
     public static void main(String[] args) {
         B obj1 = new B();
         System.out.println(B.meth());
